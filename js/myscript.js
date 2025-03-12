c223d7677826cf89c27e0e64a23e4bdb76e4d647
emperorbenz1 <ogologabriel02@gmail.com>
2025-03-12 17:37:54 +0100

    java special

---
+++ js/myscript.js
@@ -0,0 +1,44 @@
+
+// this is used to alert message to user when my website loads
+// window.alert("I really love PIZZA")
+
+// COMMENT IN JAVASCRIPT
+
+// this is a single line comment
+
+
+/*This
+is
+a
+multiline
+comment
+*/
+
+// VARIABLES IN JAVASCRIPT
+
+/*
+    A variable is a container used for storing single data.
+
+    Two steps in naming a variable in javascript:
+
+    1. Declaration (var, let, const);
+    2. Assignment (= assignment operator)
+
+    Strings, Numbers, Boolean, Null, Undefined
+
+    */
+
+let myname = 'miracle';
+const firstname = "chidozie"; //strings Datatype
+var age = 21; // integer datatype
+var count = 22.1; // float datatype
+let ages = "21"; // string datatype '21' + 1 =211
+let student = false; // boolean datatype
+let k = null; // Null datatype
+let country; //undefined datatype
+
+// firstname = 'Angel';
+
+console.log(myname)
+console.log(firstname)
+console.log(age)
